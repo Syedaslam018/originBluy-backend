7c5b77b1daae8b56e43eac738c39ea663ffc41bc
Syed Aslam <syedaslam@Syeds-MacBook-Air.local>
2025-03-01 23:27:14 +0530

    bug fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 9000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI, {
